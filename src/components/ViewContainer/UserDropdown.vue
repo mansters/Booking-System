f192ef8f59e986b1aa96369741e706481f196201
Manny <manny.mengxb@gmail.com>
2019-05-05 17:20:48 +0800

    角标替换

--- src/components/ViewContainer/UserDropdown.vue
+++ src/components/ViewContainer/UserDropdown.vue
@@ -1,6 +1,8 @@
 <template>
   <el-submenu index="mine">
-    <template slot="title">我的</template>
+    <template slot="title">
+      <span class="iconfont icon-caidan07" style="font-size: 36px;"></span>
+    </template>
     <el-menu-item index="2-1">尊敬的, {{loginUser.username}}</el-menu-item>
     <el-menu-item index="membership" @click="membershipDialogVisible = true">我的会员</el-menu-item>
     <el-menu-item index="2-3">我的订单</el-menu-item>
